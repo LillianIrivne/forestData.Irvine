38aefd5f7302bddedbc6c49dc9b5a444d6affe1d
LillianIrivne <123099877+LillianIrivne@users.noreply.github.com>
2024-02-28 19:04:17 -0500

    Update forestData-1.js

--- forestData-1.js
+++ forestData-1.js
@@ -28,15 +28,17 @@ let parks = [
 
 // Task 1: Using array and object manipulation, change the name of "Riverside Park" to "Riverside Greenspace" for the relevant tree in the Parks Array.
 // Your code here 
-//  name: "Riverside Greenspace",
+//  parks.find(park => park.name === "Riverside Park").name = "Riverside Greenspace";
 
 // Task 2: Utilize array and object manipulation to change the name of the tree species "Maple" to "Sugar Maple" for the relevant tree located in "Central Park" within the parks array.
 // Your code here
-//  { species: "Sugar Maple", age: 5, health: "Good", height: 20 },
+//  parks.find(park => park.name === "Central Park").trees.find(tree => tree.species === "Maple").species = "Sugar Maple";
+
 
 // Task 3: Employ array and object manipulation to add a new tree with species "Birch", age 7, health "Good", location "Central Park", and height 18 to the parks array.
 // Your code here
-// { species: "Birch", age: 7, health: "Good", height: 18 },
+// parks.find(park => park.name === "Central Park").trees.push({ species: "Birch", age: 7, health: "Good", height: 18 });
+
 
 // Task 4: Using array and object manipulation, retrieve a list of all tree species located in "Central Park". Store the list in a variable named centralParkTrees.
 // Your code here
@@ -44,6 +46,18 @@ let parks = [
 
 // Task 5: Use code to calculate and store the average age of all the trees in the parks array into a variable named averageTreeAge.
 // Your code here
+//let totalAge = 0;
+//let totalTrees = 0;
+
+//parks.forEach(park => {
+//    park.trees.forEach(tree => {
+//        totalAge += tree.age;
+//        totalTrees++;
+//    });
+//});
+//let averageTreeAge = totalAge / totalTrees;
+//console.log("Average Tree Age:", averageTreeAge);
+
 
 // Task 6: Use code to determine which tree is the tallest among all the trees in the parks array. Store the tallest tree in a variable named tallestTree.
 // Your code here
